b822e0933b5bbc8d5a1f55cb1d9ee57da1174ce7
KGrama <gbkeshava@gmail.com>
2021-11-09 22:00:52 +0000

    Switch to caffeine cache

---
+++ src/main/java/com/github/kgrama/apiwrapperdemo/config/CacheConfiguration.java
@@ -0,0 +1,11 @@
+package com.github.kgrama.apiwrapperdemo.config;
+
+import org.springframework.context.annotation.Bean;
+
+public class CacheConfiguration  {
+	/*@Bean
+	public Caffeine caffeineConfig() {
+	    return Caffeine.newBuilder().expireAfterWrite(60, TimeUnit.MINUTES);
+	}*/
+}
+
